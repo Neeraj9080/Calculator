92c3e519953729427bce6d0a5bd4bee63b9b43f4
Neeraj9080 <neerajpatel02468@gmail.com>
2025-01-07 12:53:26 +0530

    Addition in Add branch

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,26 @@ import java.util.Scanner;
       System.out.println("Enter Second Number");
       num2 = input.nextInt();
       
+     System.out.println("Choose Operation");
+     System.out.println("1.Addition : \n2.Subtraction : \n3.Multiplication : \n4.Division: \n5.Exit");
+     int num3;
+     num3 = input.nextInt();
+     int result;
      
+     switch(num3)
+     {
+          case 1:
+          {
+               result = num1 + num2;
+               System.out.println(result);
+               break;
+           }
+           default:
+           {
+            System.out.println("Please Enter valid input");
+           }
+     }
+
    }
 }
 
