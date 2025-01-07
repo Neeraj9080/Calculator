a4a1497bd9f21a0e5b1b774a80e386d90e7b9c2b
Neeraj9080 <neerajpatel02468@gmail.com>
2025-01-07 12:15:59 +0530

    Message passing

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class Calculator
+ {
+   public static void main(String args[])
+   {
+      System.out.println("Welcome to Calculator");
+      Scanner input = new Scanner(System.in);
+      int num1,num2;
+      System.out.println("Enter First Number");
+      num1 = input.nextInt();
+      System.out.println("Enter Second Number");
+      num2 = input.nextInt();
+      
+     
+   }
+}
+
